e744893f31a317b772cb0c8c55a66d6fa777e8f3
Ayhan Gad <16155924@mersin.edu.tr>
2024-03-26 14:31:58 +0300

    Hero section completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 const Home = () => {
     return (
         <>
-            <section>
-                Home
+            <section className='hero'>
+                <img src="../src/assets/images/Hero.jpg" alt="" />
+                <div className="lockup">
+                    <div className="wrapper">
+                        <div className="badge">
+                            BİOCODER TEKNOLOJİ A.Ş.
+                        </div>
+                        <h1 className="lockup-text">
+                            Profesyonel Çözümlerle Teknolojiyi Yeniden Şekillendirmek.
+                        </h1>
+                        <p className="lockup-information">
+                            BioCoder Teknoloji, güçlü gömülü sistem ekibiyle endüstri standartlarına uygun yazılım geliştirme ve inovatif çözümler sunma konusunda uzmanlaşmıştır.
+                        </p>
+                        <div className="button">
+                            <i className="ri-mail-line"></i>
+                            <span className="label-wrapper">
+                                İletişime geç
+                            </span>
+                        </div>
+                    </div>
+                </div>
             </section>
         </>
     )
