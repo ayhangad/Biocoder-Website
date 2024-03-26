2824e768a162bba894c8b918b369d7b7ff442c1f
Ayhan Gad <16155924@mersin.edu.tr>
2024-03-26 15:17:32 +0300

    Some modifications after Hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,28 +2,32 @@ import React from 'react'
 const Home = () => {
     return (
         <>
-            <section className='hero'>
-                <img src="../src/assets/images/Hero.jpg" alt="" />
-                <div className="lockup">
-                    <div className="wrapper">
-                        <div className="badge">
-                            BİOCODER TEKNOLOJİ A.Ş.
-                        </div>
-                        <h1 className="lockup-text">
-                            Profesyonel Çözümlerle Teknolojiyi Yeniden Şekillendirmek.
-                        </h1>
-                        <p className="lockup-information">
-                            BioCoder Teknoloji, güçlü gömülü sistem ekibiyle endüstri standartlarına uygun yazılım geliştirme ve inovatif çözümler sunma konusunda uzmanlaşmıştır.
-                        </p>
-                        <div className="button">
-                            <i className="ri-mail-line"></i>
-                            <span className="label-wrapper">
-                                İletişime geç
-                            </span>
+                <div className='hero'>
+                    <img src="../src/assets/images/Hero.jpg" alt="" />
+                    <div className="lockup">
+                        <div className="wrapper">
+                            <div className="badge">
+                                BİOCODER TEKNOLOJİ A.Ş.
+                            </div>
+                            <h1 className="lockup-text">
+                                Profesyonel Çözümlerle Teknolojiyi Yeniden Şekillendirmek.
+                            </h1>
+                            <p className="lockup-information">
+                                BioCoder Teknoloji, güçlü gömülü sistem ekibiyle endüstri standartlarına uygun yazılım geliştirme ve inovatif çözümler sunma konusunda uzmanlaşmıştır.
+                            </p>
+                            <div className="button">
+                                <i className="ri-mail-line"></i>
+                                <span className="label-wrapper">
+                                    İletişime geç
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </section>
+                <div className='products'>
+                    Works
+                </div>
+
         </>
     )
 }
