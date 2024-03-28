28be82834ff45714aee18f56420bbff51173fd7b
Ayhan Gad <16155924@mersin.edu.tr>
2024-03-28 16:15:36 +0300

    Blog Page finished

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -9,6 +9,68 @@ const Blog = () => {
           <p>2004 yılında "En yeni iletişim ve bilgisayar teknolojilerini kullanarak Eğitim Teknolojileri müşterilerine dünya standartlarında çözümler sunmak" ilkesiyle yola çıkan BioCoder Teknoloji</p>
         </div>
       </section>
+      <section className="blog-section">
+        <div className="blog-item">
+          <img src="https://source.unsplash.com/random/?tech" alt="" />
+          <div className='blog-item-wrapper'>
+            <div>
+              <h4>Move rectangle pencil star export</h4>
+              <span>26 Ocak 2022</span>
+            </div>
+            <button className="button button-secondary">Devamını oku</button>
+          </div>
+        </div>
+        <div className="blog-item">
+          <img src="https://source.unsplash.com/random/?technology" alt="" />
+          <div className='blog-item-wrapper'>
+            <div>
+              <h4>Move rectangle pencil star export</h4>
+              <span>26 Ocak 2022</span>
+            </div>
+            <button className="button button-secondary">Devamını oku</button>
+          </div>
+        </div>
+        <div className="blog-item">
+          <img src="https://source.unsplash.com/random/?programming" alt="" />
+          <div className='blog-item-wrapper'>
+            <div>
+              <h4>Move rectangle pencil star export</h4>
+              <span>26 Ocak 2022</span>
+            </div>
+            <button className="button button-secondary">Devamını oku</button>
+          </div>
+        </div>
+        <div className="blog-item">
+          <img src="https://source.unsplash.com/random/?internet" alt="" />
+          <div className='blog-item-wrapper'>
+            <div>
+              <h4>Move rectangle pencil star export</h4>
+              <span>26 Ocak 2022</span>
+            </div>
+            <button className="button button-secondary">Devamını oku</button>
+          </div>
+        </div>
+        <div className="blog-item">
+          <img src="https://source.unsplash.com/random/?electronics" alt="" />
+          <div className='blog-item-wrapper'>
+            <div>
+              <h4>Move rectangle pencil star export</h4>
+              <span>26 Ocak 2022</span>
+            </div>
+            <button className="button button-secondary">Devamını oku</button>
+          </div>
+        </div>
+        <div className="blog-item">
+          <img src="https://source.unsplash.com/random/?computer" alt="" />
+          <div className='blog-item-wrapper'>
+            <div>
+              <h4>Move rectangle pencil star export</h4>
+              <span>26 Ocak 2022</span>
+            </div>
+            <button className="button button-secondary">Devamını oku</button>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
