91a798e70e74fadb66bd34682e6bd05fa60a5b68
Ayhan Gad <16155924@mersin.edu.tr>
2024-04-19 16:04:44 +0300

    Responsive finished

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -76,7 +76,7 @@ const Header = () => {
                     })
                 }
                 <div className="nav-trailing">
-                    <Button type={'button'} disabled={false} innerText={'Giriş yap'} iconR={'arrow-right-up'} />
+                    <Button type={'button button-secondary'} disabled={false} innerText={'İletişime geç'} />
                 </div>
                 <div className="nav-trailing-responsive">
                     <Button type={'button button-secondary'} disabled={false} innerText={'Menu'} iconL={'menu'} />
