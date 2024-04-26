188e72c911c18640a2992cbeb5770ac5c4943ddb
Hakan Tapanyiğit <hakantapanyigit@hotmail.com>
2024-04-26 17:32:52 +0300

    feat: carousel

--- src/components/shared/Carousel.jsx
+++ src/components/shared/Carousel.jsx
@@ -1,17 +1,71 @@
-import React from 'react'
-const Carousel = ({ data = [] }) => {
-    console.log(data)
-    return (
-        <div className="carousel">
-            <div className="carousel-inner">
-                {data.map((item, index) => (
-                    <div key={index} className="carousel-item">
-                        fdsfsd
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
-}
+import { useState, useEffect } from 'react';
+import Button from './Button'
+import heroData from '../../data/sliderData.json'
 
-export default Carousel
+const Carousel = () => {
+    const [index, setIndex] = useState(0);
+    const updateHeroContent = () => {
+        setIndex((index + 1) % heroData.length);
+    };
+    useEffect(() => {
+        const interval = setInterval(updateHeroContent, 5000);
+        return () => clearInterval(interval);
+    }, [index, heroData]);
+
+    const mailButton = () => {
+        const mailtoLink = `mailto:info@biocoder.com.tr`;
+        window.location.href = mailtoLink;
+    };
+
+    return (
+        <section className="hero">
+            <img src={heroData[index].image} alt=""/>
+            <div className="lockup">
+                <div className="wrapper">
+                    <div className="badge">
+                        BİOCODER TEKNOLOJİ A.Ş.
+                    </div>
+                    <h1 className="lockup-text">
+                        {heroData[index] && heroData[index].title}
+                    </h1>
+                    <p className="lockup-information">
+                        {heroData[index] && heroData[index].description}
+                    </p>
+                    <Button onClick={mailButton} type={'button'} disabled={false} innerText={'İletişime geç'}
+                            iconL={'mail'}/>
+                </div>
+            </div>
+            <div className="floating-menu">
+                <a href='https://bee.biocoder.com.tr/' target='_blank' className="floating-item jordy-blue-500">
+                    <i className='ri-bug-fill'></i>
+                    <span>
+                        Kovan Takip <i className='ri-arrow-right-up-line'></i>
+                    </span>
+                </a>
+                <a href='https://portal.biocoder.com.tr/Account/Login' target='_blank'
+                   className="floating-item jordy-blue-600">
+                    <i className='ri-temp-hot-fill'></i>
+                    <span>
+                        Sıcaklık Nem Takip <i className='ri-arrow-right-up-line'></i>
+                    </span>
+                </a>
+                <a href='http://logistic.biocoder.com.tr/' target='_blank' className="floating-item jordy-blue-700">
+                    <i className='ri-map-pin-4-fill'></i>
+                    <span>
+                        GSM Datalogger <i className='ri-arrow-right-up-line'></i>
+                    </span>
+                </a>
+            </div>
+            <div className="slider-arrows">
+                <div className="arrow left" onClick={() => updateHeroContent(-1)}>
+                    <i className="ri-arrow-left-line"></i>
+                </div>
+                <div className="arrow right" onClick={() => updateHeroContent(1)}>
+                    <i className="ri-arrow-right-line"></i>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Carousel;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,62 +1,16 @@
 import React from 'react'
-import Hero from '../assets/images/Hero.jpg'
 import Button from '../components/shared/Button'
 import ProductsData from '../data/products.json'
 import { useNavigate } from 'react-router-dom'
 import Carousel from "../components/shared/Carousel.jsx";
-import sliderData from "../data/sliderData.json";
 const Home = () => {
     const navigate = useNavigate();
-    const mailButton = () => {
-        const mailtoLink = `mailto:info@biocoder.com.tr`;
-        window.location.href = mailtoLink;
-    };
+
 
     return (
 
         <>
-            <Carousel data={sliderData?.sections} />
-
-            <section className='hero'>
-                <img src={Hero} alt=""/>
-                <div className="lockup">
-                    <div className="wrapper">
-                        <div className="badge">
-                            BİOCODER TEKNOLOJİ A.Ş.
-                        </div>
-                        <h1 className="lockup-text">
-                            Profesyonel Çözümlerle Teknolojiyi Yeniden Şekillendirmek.
-                        </h1>
-                        <p className="lockup-information">
-                            BioCoder Teknoloji, güçlü gömülü sistem ekibiyle endüstri standartlarına uygun yazılım
-                            geliştirme ve inovatif çözümler sunma konusunda uzmanlaşmıştır.
-                        </p>
-                        <Button onClick={mailButton} type={'button'} disabled={false} innerText={'İletişime geç'}
-                                iconL={'mail'}/>
-                    </div>
-                </div>
-                <div className="floating-menu">
-                    <a href='https://bee.biocoder.com.tr/' target='_blank' className="floating-item jordy-blue-500">
-                        <i className='ri-bug-fill'></i>
-                        <span>
-                            Kovan Takip <i className='ri-arrow-right-up-line'></i>
-                        </span>
-                    </a>
-                    <a href='https://portal.biocoder.com.tr/Account/Login' target='_blank'
-                       className="floating-item jordy-blue-600">
-                        <i className='ri-temp-hot-fill'></i>
-                        <span>
-                            Sıcaklık Nem Takip <i className='ri-arrow-right-up-line'></i>
-                        </span>
-                    </a>
-                    <a href='http://logistic.biocoder.com.tr/' target='_blank' className="floating-item jordy-blue-700">
-                        <i className='ri-map-pin-4-fill'></i>
-                        <span>
-                            GSM Datalogger <i className='ri-arrow-right-up-line'></i>
-                        </span>
-                    </a>
-                </div>
-            </section>
+            <Carousel />
             <section className='products'>
                 <div className="section-header">
                     <h1>
@@ -72,6 +26,7 @@ const Home = () => {
                     {
                         ProductsData?.sections?.slice(0, 4).map((product, index) => {
                             return (
+                                // eslint-disable-next-line react/jsx-key
                                 <div className="product-item">
                                     <img src={product?.image} alt=""/>
                                     <div className="product-information">
